1b5e40c8f8d7de6784cdbb6ced57538a14ba5455
robinb389 <rsingh@inscastellet.cat>
2025-01-17 18:16:52 +0100

    comments

--- robin_tasca8b/src/robin_tasca8b/Tasca8b_1.java
+++ robin_tasca8b/src/robin_tasca8b/Tasca8b_1.java
@@ -1,6 +1,6 @@
 // Fes un programa que demani per pantalla la mida n de la matriu i generi una matriu de n x n i l’ompli de la següent manera: La posició (i, j) de la matriu ha de contenir i+j (la suma dels índex).  Exemple matriu[1][3] = 4 . Un cop construïda la matriu cal mostrar-la per pantalla. Si la mida n és 4 hauria de sortir això:
 package robin_tasca8b;
-
+// this is scanner
 import java.util.Scanner;
 // class
 public class Tasca8b_1 {
